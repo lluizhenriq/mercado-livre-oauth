c4c8d69111c84a3e22fd403a77df34fa85a2a85b
lluizhenriq <luizhenriqu9999@gmail.com>
2026-08-15 16:11:12 -0300

    Update response messages in app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Mega Desconto - servidor online!"
+    return "Mega Desconto - bot online!"
+
 
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
 
     if not code:
-        return "Callback funcionando, mas nenhum código foi recebido."
+        return "Callback funcionando!"
 
-    return f"Código recebido: {code}"
+    return "Autorização recebida com sucesso!"
+
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
